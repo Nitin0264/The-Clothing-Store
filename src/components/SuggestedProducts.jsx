b58a96f88ff148d3f630180f46f3c764d17a0884
Nitin <nc8224140@gmail.com>
2026-02-25 13:35:34 +0530

    created the suggestion project inside a single project

---
+++ src/components/SuggestedProducts.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+
+// function SuggestedProducts({ index, image, id, name }) {
+//   return (
+//     <Link to={`/products/${id}`} key={index} className='h-[320px] w-[220px] '>
+
+//       <img src={image} alt="Products" />
+//       <h1>{name}</h1>
+//       {/* <h1>hey everyone whats up</h1> */}
+//       {/* creating the parems here to pass  */}
+//     </Link >
+
+//   )
+// }
+// export default SuggestedProducts
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,39 +1,49 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { userContext } from '../context/UserContext'
+import ProductsDisplay from '../components/ProductsDisplay'
+// import SuggestedProducts from '../components/SuggestedProducts'
 
 function Product() {
   let { pid } = useParams()
   const [image, setImage] = useState(null)
   const [product1, setProduct1] = useState(false)
+  const [suggestedProduct,setSuggestedProduct] = useState([])
   const { products } = useContext(userContext)
   const [size1,setSize1]= useState()
 
   useEffect(() => {
     const product = products.find(p => p._id === pid);
+    const otherProducts = products.filter( i=> i._id!=product._id)
+     const random = [...otherProducts.sort(()=>0.5-Math.random()).slice(4,8)]
     // let size = product.sizes.map(i=>{return i})
     // console.log(size)                                            
-    if (product && product.sizes) {
+    if (product && product.sizes && random) {
+      setSuggestedProduct(random)
       setSize1(product.sizes)
       setImage(product.image[0])
       setProduct1(product)
         }
 
 
-  }, [products])
+  }, [products,pid])
    
   return (
-    <div className='flex gap-[10px] w-[100%] mx-auto p-[20px] rounded-md'>
+
+    //  <Link to={`/products/${id}`} key={index} className='h-[320px] w-[220px] '></Link>
+
+    <div>
+    <div className='flex gap-[15px] w-[100%] mx-auto p-[20px] rounded-md'>
       <div className='flex-1'>
-       <div className='flex gap-2'>
+       <div className='flex gap-5'>
         <div className='sideImage flex flex-col w-[18%]'>
           {
-            product1 && product1.image.map((i)=>(
+            product1 && product1.image.map((i,index)=>( <div key={index} className='my-2 mt-[10px]'>
               <img className='my-2 mx-2' src={i} />
-            ))
+          </div>  ))
           }
           </div> 
-        <div className='Image w[80%]'>
+        <div className='Image w[80%] mt-[10px]'>
            <img src={image} alt='Product' /></div> 
        </div>
       </div>
@@ -61,7 +71,7 @@ function Product() {
       
         <button  className='mt-5 bg-black text-white h-[40px] w-[120px] rounded-md pointer'>Add to cart</button>
 
-        <ol className='mt-5 flex flex-col gap-3'>
+        <ol className='mt-5 flex flex-col gap-3 list-disc'>
           <li>100% genuine and authentic.</li>
           <li>Cash on Delivery (COD) available.</li>
           <li> Premium quality.</li>
@@ -69,7 +79,23 @@ function Product() {
         </ol>
 
       </div>
+
     </div>
+    {/* second div */}
+        <h3 className='text-center text-gray-500 text-3xl my-[40px] '>You May also like this</h3>
+      <div className='flex gap-3 my-[10px]'>
+        {
+           suggestedProduct.map((data,index)=> <div key={index}>
+           <ProductsDisplay  image={data.image[0]} name={data.name} />
+           
+            {/* <img key={index} src ={data.image[0]} alt={data.name} />
+            <p>{data.name}</p> */}
+           </div>)
+        }
+      </div>
+
+    </div>
+
   )
 }
 
